9d9566ed823b7c60d510045a313f61423b8882d1
krook98 <sokalski.jakub98@gmail.com>
2022-07-17 23:25:55 +0200

    Inner class functionality added

--- src/Album.java
+++ src/Album.java
@@ -2,47 +2,43 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Album {
-    // The albums will be stored in an ArrayList
-    // Songs from different albums can be added to the playlist and will appear in the list in the order
-    // they are added.
+    // Modify the playlist challenge so that the Album class uses an inner class.
+    // Instead of using an ArrayList to hold its tracks, it will use an inner class called SongList
+    // The inner SongList class will use an ArrayList and will provide a method to add a song.
+    // It will also provide findSong() methods which will be used by the containing Album class
+    // to add songs to the playlist.
+    // Neither the Song class or the Main class should be changed.
+
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
+
 
     public boolean addSong(String title, double duration) {
-        if(findSong(title) == null) {
-            this.songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
+        return this.songs.add(new Song(title, duration));
     }
 
-    private Song findSong(String title) {
-        for(Song checkedSong: this.songs) {
-            if(checkedSong.getTitle().equals(title)) return checkedSong;
-        }
-        return null;
-    }
 
     public boolean addToPlaylist(int songNumber, LinkedList<Song> playlist) {
-        int index = songNumber - 1;
-        if((index >= 0) && (index <= this.songs.size())) {
-            playlist.add(this.songs.get(index));
+        Song checkedSong = this.songs.findSong(songNumber);
+        if(checkedSong != null) {
+            playlist.add(checkedSong);
             return true;
         }
+
         System.out.println("Song " + songNumber + " is not on this album.");
         return false;
     }
 
     public boolean addToPlaylist(String title, LinkedList<Song> playlist) {
-        Song song = findSong(title);
+        Song song = this.songs.findSong(title);
         if(song != null) {
             playlist.add(song);
             return true;
@@ -51,4 +47,33 @@ public class Album {
         return false;
     }
 
+    public class SongList {
+        private ArrayList<Song> songs;
+
+        public SongList() {
+            this.songs = new ArrayList<Song>();
+        }
+
+        private boolean add(Song song) {
+            if (songs.contains(song)) {
+                return false;
+            }
+            this.songs.add(song);
+            return true;
+        }
+
+        private Song findSong(String title) {
+            for (Song checkedSong : this.songs) {
+                if (checkedSong.getTitle().equals(title)) return checkedSong;
+            }
+            return null;
+        }
+
+        public Song findSong(int number) {
+            int index = number - 1;
+            if (index >= 0 && index < songs.size()) return songs.get(index);
+            else return null;
+
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,12 @@ public class Main {
     private static ArrayList<Album> albums = new ArrayList<Album>();
 
     public static void main(String[] args) {
-        // Create a program that implements a playlist for songs
-        // Create a Song class having Title and Duration for a song.
-        // The program will have an Album class containing a list of songs.
-        // The albums will be stored in an ArrayList
-        // Songs from different albums can be added to the playlist and will appear in the list in the order
-        // they are added.
-        // Once the songs have been added to the playlist, create a menu of options to:-
-        // Quit,Skip forward to the next song, skip backwards to a previous song.  Replay the current song.
-        // List the songs in the playlist
-        // A song must exist in an album before it can be added to the playlist (so you can only play songs that
-        // you own).
-        // Hint:  To replay a song, consider what happened when we went back and forth from a city before we
-        // started tracking the direction we were going.
-        // As an optional extra, provide an option to remove the current song from the playlist
-        // (hint: listiterator.remove()
+        // Modify the playlist challenge so that the Album class uses an inner class.
+        // Instead of using an ArrayList to hold its tracks, it will use an inner class called SongList
+        // The inner SongList class will use an ArrayList and will provide a method to add a song.
+        // It will also provide findSong() methods which will be used by the containing Album class
+        // to add songs to the playlist.
+        // Neither the Song class or the Main class should be changed.
 
         Album album = new Album("Blackwater Park", "Opeth");
         album.addSong("The Lepper Affinity", 10.23);
@@ -71,7 +62,7 @@ public class Main {
             int action = scanner.nextInt();
             scanner.nextLine();
 
-            switch (action) {
+            switch(action) {
                 case 0:
                     System.out.println("Playlist complete.");
                     quit = true;
@@ -85,10 +76,12 @@ public class Main {
                     }
                     if(listIterator.hasNext()) {
                         System.out.println("Now playing " + listIterator.next().toString());
+                    } else {
+                        System.out.println("We have reached the end of the playlist");
+                        forward = false;
                     }
-                    System.out.println("You have reached the end of the playlist");
-                    forward = false;
                     break;
+
                 case 2:
                     if(forward) {
                         if(listIterator.hasPrevious()) {
@@ -98,11 +91,27 @@ public class Main {
                     }
                     if(listIterator.hasPrevious()) {
                         System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("We are at the start of the playlist");
+                        forward = true;
                     }
-                    System.out.println("You have reached the beginning of the playlist");
-                    forward = true;
                     break;
                 case 3:
+                    if(forward) {
+                        if(listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("We are at the start of the list");
+                        }
+                    } else {
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
                     break;
                 case 4:
                     printList(playlist);
@@ -110,8 +119,17 @@ public class Main {
                 case 5:
                     printMenu();
                     break;
+
                 case 6:
-                    if(playlist.size() > 0) listIterator.remove();
+                    if(playlist.size() >0) {
+                        listIterator.remove();
+                        if(listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if(listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
+                    break;
 
             }
         }
